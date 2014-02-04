e27aa125fe639ce5973d866ca8b2875ba1fa5116
CLDP <muyunhai@gmail.com>
2014-02-04 14:49:00 -0500

    Create 1273.cpp

---
+++ Volume-3/1273.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+const int MAXN = 210;
+const int INF = 1000000000;
+
+vector<pair<int, int> > x[MAXN];
+
+int max_flow(int s, int t, int n) {
+    vector<int> ff(n + 1, 0);
+    vector<int> fa(n + 1, 0);
+    vector<int> h(n + 1, 0);
+    vector<int> hh(n + 1, 0);
+    vector<int> fi(n + 1, 0);
+    
+    int i = 0, now, ans = 0;
+    hh[0] = n;
+    
+    i = s;
+    now = INF;
+    
+    while (h[s] < n) {
+        ff[i] = now;
+        bool flag = false;
+        vector<pair<int, int> >::iterator p = x[i].begin();
+        while (p != x[i].end()) {
+            if (p->second > 0 && h[p->first] + 1 == h[i]) {
+                flag = true;
+                if (p->second < now) now = p->second;
+                fa[p->first] = i;
+                fi[p->first] = p - x[i].begin();
+                i = p->first;
+                if (i == t) {
+                    ans += now;
+                    while (i != s) {
+                        int tmp = i;
+                        i = fa[i];
+                        p = x[i].begin() + fi[tmp];
+                        p->second -=now;
+                        x[tmp].push_back(make_pair(i, now));
+                    }
+                    now = INF;
+                }
+                break;
+            }
+            ++p;
+        }
+        if (flag) continue;
+        
+        int min = n-1;
+        p = x[i].begin();
+        while (p != x[i].end()) {
+            if (p->second > 0 && h[p->first] < min) min = h[p->first];
+            ++p;
+        }
+        
+        --hh[h[i]];
+        if (hh[h[i]] == 0) break;
+        h[i] = min + 1;
+        ++hh[h[i]];
+        if (i != s) {
+            i = fa[i];
+            now = ff[i];
+        }
+    }
+    
+    return ans;
+}
+
+int main() {
+    int M, N;
+    while (cin >> M >> N) {
+        for (int i = 1; i <= N; ++i) x[i].clear();
+        for (int i = 0; i < M; ++i) {
+            int a, b, c;
+            cin >> a >> b >> c;
+            x[a].push_back(make_pair(b, c));
+        }
+        cout << max_flow(1, N, N) << endl;
+    }
+    return 0;
+}
