3dc76d53efa72828174929950408bdcee903383d
CLDP <muyunhai@gmail.com>
2014-02-11 16:25:06 -0500

    Create 1861.cpp

---
+++ Volume-9/1861.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+const int MAXN = 1001;
+const int MAXM = 20000;
+const int INF = 1000000000;
+
+struct line {
+    int a, b, c;
+    bool operator < (line const &B) const {
+        return c < B.c;
+    }
+};
+
+line y[MAXM];
+int d[MAXN], f[MAXN];
+
+
+int find(int a) {
+    if (a != f[a]) f[a] = find(f[a]);
+    return f[a];
+}
+
+int Union(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a == b) return 0;
+    if (d[a] > d[b]) f[b] = f[a]; else f[a] = f[b];
+    d[a] += d[b];
+    d[b] = d[a];
+    return 0;
+}
+
+
+
+int main() {
+    int N, M;
+    scanf("%d%d", &N, &M);
+    for (int i = 0; i < M; ++i) cin >> y[i].a >> y[i].b >> y[i].c;
+    sort(y, y + M);
+    
+    for (int i = 1; i <= N; ++i) {
+        d[i] = 1;
+        f[i] = i;
+    }
+    int ans = 0;
+    vector<pair<int, int> > ansT;
+    for (int i = 0; i < M; ++i) {
+        int a = y[i].a, b = y[i].b, c = y[i].c;
+        a = find(a), b = find(b);
+        if (a == b) continue;
+        if (c > ans) {
+            ans = c;
+            
+        }
+        ans = max(ans, c);
+        Union(a, b);
+        ansT.push_back(make_pair(y[i].a, y[i].b));
+    }
+    
+    cout << ans << endl;
+    cout << ansT.size() << endl;
+    for (int i = 0; i < ansT.size(); ++i) cout << ansT[i].first << " " << ansT[i].second << endl;
+    
+    return 0;
+}
